ca4eaeb1afeb92cc4e02010108863f16f640c406
Nguyen Cong Huy <nguyenconghuy@sutunam.com>
2020-02-20 19:26:53 +0700

    14 - Upgrade woocommerce plugin

--- wp-content/plugins/woocommerce/templates/single-product.php
+++ wp-content/plugins/woocommerce/templates/single-product.php
@@ -11,7 +11,6 @@
  * the readme will list any important changes.
  *
  * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
  * @package 	WooCommerce/Templates
  * @version     1.6.4
  */
@@ -56,4 +55,6 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_sidebar' );
 	?>
 
-<?php get_footer( 'shop' ); ?>
+<?php get_footer( 'shop' );
+
+/* Omit closing PHP tag at the end of PHP files to avoid "headers already sent" issues. */
